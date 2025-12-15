91227fd9d9054b3cb041eaee050c0cc66cf5c64b
Gilles MAMBIDI <gilles.mambidi.keya@etud.u-picardie.fr>
2025-12-15 23:59:21 +0100

    update the vite file

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',//'/Business-Omnibus/',  <--- THIS IS CRITICAL ./dist
-  //<--! build : {
-   // outDir: 'docs'// Repertoire de deploiement.
-  //}-->
+  base: '/Business-Omnibus/', 
+  build: {
+    // Ensure this defaults to 'dist' so the action finds the folder
+    // You can remove the 'outDir' line entirely if you want
+  }
 });
